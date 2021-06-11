b440effe980cab9a80c0449b073030f0a1d09503
Wojciech Wysocki <voytek83@gmail.com>
2021-06-11 15:39:03 +0200

    init

--- src/com/wojtek/TransactionAnalyzerSimple.java
+++ src/com/wojtek/TransactionAnalyzerSimple.java
@@ -5,6 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class TransactionAnalyzerSimple {
@@ -14,13 +17,18 @@ public class TransactionAnalyzerSimple {
         final Path path = Paths.get(RESOURCES + args[0]);
         final List<String> lines = Files.readAllLines(path, StandardCharsets.ISO_8859_1);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for (final String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if(date.getMonth() == Month.JANUARY){
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+
         }
 
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total for all transactions in January is " + total);
 
     }
 }
